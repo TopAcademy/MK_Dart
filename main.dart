ae70bc3d926c0432116284baf956d9e88aac7f3f
Maxim Huzmiev <max@huzm.ru>
2025-02-12 20:09:17 +0000

    Several constructors added for Point

--- main.dart
+++ main.dart
@@ -1,6 +1,15 @@
 import "point.dart";
 
 void main() {
-  var p = Point(10, 20);
-  p.show();
+  var p1 = Point();
+  var p2 = Point(10, 25);
+  var p3 = Point.empty();
+  var p4 = Point.fromXY(100, 200);
+  var p5 = Point.fromCopy(p4);
+
+  p1.show();
+  p2.show();
+  p3.show();
+  p4.show();
+  p5.show();
 }
--- point.dart
+++ point.dart
@@ -1,14 +1,27 @@
 /// Point class module
 
 class Point {
-  int x;
-  int y;
+  // Init fields here by implicit default constructor
+  int _x = 0;
+  int _y = 0;
 
-  // Constructor
-  Point(this.x, this.y);
+  // Constructor with XY params
+  Point([int? new_x, int? new_y]) {
+    _x = new_x ?? 0;
+    _y = new_y ?? 0;
+  }
+
+  // Named pseudo-default constructor
+  Point.empty() : _x = 0, _y = 0;
+
+  // Named constructor from XY
+  Point.fromXY(int new_x, int new_y) : _x = new_x, _y = new_y;
+
+  // Named copy constructor
+  Point.fromCopy(Point p2) : _x = p2._x, _y = p2._y;
 
   // Print on console
   void show() {
-    print("X = $x; Y = $y");
+    print("X = $_x; Y = $_y");
   }
 }
